04011062b425f55491b50112fafa8a393cef5adf
17789612632@163.com <17789612632@163.com>
2018-10-26 16:02:59 +0800

    "添加启动类，进行扫描"

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -0,0 +1,23 @@
+package com.xuecheng.manage_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 作者: lin
+ * 描述:
+ * 日期: 2018/10/26 14:58
+ * 扫描接口api中的接口和model的实体
+ */
+@SpringBootApplication
+@EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
+@ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
+@ComponentScan(basePackages={"com.xuecheng.manage_cms"})//扫描本项目下的所有类
+public class ManageCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageCmsApplication.class,args);
+    }
+}
